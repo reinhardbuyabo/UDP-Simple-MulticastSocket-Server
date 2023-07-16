e135d0ac49fe431c31c6409d029e3c49947e017a
Reinhard <buyabor@gmail.com>
2023-07-16 16:07:21 +0100

    refactor: added commenting

--- Client.java
+++ Client.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * This code represents a client for a UDP multicast server.
+ * The client joins a multicast group, receives messages from the server, and logs them.
+ */
+
 package com.mytrial.udpmulticastserverdemo;
 
 import java.io.IOException;
@@ -9,61 +14,63 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
-/**
- *
- * @author reinh
- */
 public class Client {
-    MulticastSocket socket = null;
-    byte[] buffer = null;
-    DatagramPacket packet = null;
-    InetAddress ip = null;
+    MulticastSocket socket = null;  // Socket for multicast communication
+    byte[] buffer = null;           // Buffer to store received data
+    DatagramPacket packet = null;   // Packet to hold received data
+    InetAddress ip = null;          // IP address of the multicast group
     
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Client client = new Client();
         client.initializeVariable();
         client.connecting();
     }
-    private void initializeVariable(){
-        try{
-            socket = new MulticastSocket(Constants.PORT);
-            ip = InetAddress.getByName(Constants.IP);
-            buffer = new byte[Constants.BUFFER_SIZE];
-        } catch(IOException ex){
+    
+    // Initializes the necessary variables for the client
+    private void initializeVariable() {
+        try {
+            socket = new MulticastSocket(Constants.PORT);  // Create a multicast socket
+            ip = InetAddress.getByName(Constants.IP);      // Get the IP address of the multicast group
+            buffer = new byte[Constants.BUFFER_SIZE];       // Create a buffer for received data
+        } catch(IOException ex) {
             log("InitializeVariable : " + ex.toString());
-        }             
+        }
     }
     
-    private String receiveData(){
+    // Receives data from the server and returns it as a string
+    private String receiveData() {
         String line = "";
         try {
-            packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
-            line = new String(packet.getData(), 0, packet.getLength());
+            packet = new DatagramPacket(buffer, buffer.length);  // Create a packet for receiving data
+            socket.receive(packet);                             // Receive data into the packet
+            line = new String(packet.getData(), 0, packet.getLength());  // Convert the received data to a string
         } catch (IOException ex) {
             log("receiveData : " + ex.toString());
         }
         return line;
     }
     
-    private void joinGroup(){
+    // Joins the multicast group specified by the IP address
+    private void joinGroup() {
         try {
-            socket.joinGroup(ip);
+            socket.joinGroup(ip);   // Join the multicast group
             log("Client Running...");
         } catch (IOException ex) {
             log("JoinGroup : " + ex.toString());
         }
     }
     
-    private void connecting(){
+    // Connects to the multicast group, receives data, and logs it indefinitely
+    private void connecting() {
         joinGroup();
-        while(true){
+        while (true) {
             String line = receiveData();
-            log("Client Recieved : " + line);
+            log("Client Received: " + line);
         }
     }
     
-    public void log(String message){
+    // Logs the given message to the console
+    public void log(String message) {
         System.out.println(message);
     }
 }
--- Server.java
+++ Server.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * This code represents a server for UDP multicast communication.
+ * The server sends messages to a multicast group based on user input.
+ */
+
 package com.mytrial.udpmulticastserverdemo;
 
 import java.io.IOException;
@@ -12,15 +17,11 @@ import java.net.MulticastSocket;
 import java.rmi.ServerException;
 import java.util.Scanner;
 
-/**
- *
- * @author reinh
- */
 public class Server {
-    MulticastSocket socket = null;
-    byte[] buffer = null;
-    DatagramSocket receive = null;
-    Scanner scan = null;
+    MulticastSocket socket = null;     // Multicast socket for sending messages
+    byte[] buffer = null;              // Buffer to hold the message
+    DatagramSocket receive = null;     // Datagram socket for receiving data
+    Scanner scan = null;               // Scanner for reading user input
     
     public static void main(String[] args) {
         Server server = new Server();
@@ -28,50 +29,54 @@ public class Server {
         server.connecting();
     }
     
-    private void initializeVariable(){
+    // Initializes the necessary variables for the server
+    private void initializeVariable() {
         try {
-            socket = new MulticastSocket();
-            buffer = new byte[Constants.BUFFER_SIZE];
-            scan = new Scanner(System.in);
+            socket = new MulticastSocket();                     // Create a multicast socket
+            buffer = new byte[Constants.BUFFER_SIZE];           // Create a buffer for the message
+            scan = new Scanner(System.in);                      // Create a scanner for user input
             
             log("Server Running...");
         } catch (ServerException ex) {
-            log("InitializeVariable : " + ex.toString());
+            log("InitializeVariable: " + ex.toString());
         } catch (IOException ex) {
-            log("InitializeVariable :" + ex.toString());
+            log("InitializeVariable: " + ex.toString());
         }
     }
     
-    private String readFromKeyboard(){
+    // Reads a line of text from the keyboard
+    private String readFromKeyboard() {
         String line = scan.nextLine();
         return line;
     }
     
+    // Sends a message to the multicast group
     private void send(String message) {
         try {
-            InetAddress ip = InetAddress.getByName(Constants.IP);
+            InetAddress ip = InetAddress.getByName(Constants.IP);   // Get the multicast group IP
             
-            buffer = message.getBytes();
-            DatagramPacket packetSend = new DatagramPacket(buffer, buffer.length, ip, Constants.PORT);
+            buffer = message.getBytes();                            // Convert the message to bytes
+            DatagramPacket packetSend = new DatagramPacket(buffer, buffer.length, ip, Constants.PORT);  // Create a packet for sending
             
-            socket.send(packetSend);
+            socket.send(packetSend);                                // Send the packet to the multicast group
             log("Message Sent");
-            
-        } catch (IOException ex){
-            log("Send : " + ex.toString());
+        } catch (IOException ex) {
+            log("Send: " + ex.toString());
         }
     }
     
-    private void connecting(){
-        while(true){
-            String data = readFromKeyboard();
-            send(data);
+    // Sends messages to the multicast group based on user input
+    private void connecting() {
+        while (true) {
+            String data = readFromKeyboard();   // Read user input
+            send(data);                         // Send the message to the multicast group
             
-            buffer = new byte[Constants.BUFFER_SIZE];
+            buffer = new byte[Constants.BUFFER_SIZE];  // Reset the buffer
         }
     }
     
-    public void log(String message){
+    // Logs the given message to the console
+    public void log(String message) {
         System.out.println(message);
     }
 }
